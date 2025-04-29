eecfc3829c293461079f72a5563795266b354bee
HudsonM64 <h8009916@gmail.com>
2025-04-29 11:31:17 -0500

    Compiled with 1 warning message, added a test file but it did not work

--- Translate/IfThenElseExp.java
+++ Translate/IfThenElseExp.java
@@ -18,7 +18,26 @@ class IfThenElseExp extends Exp {
     // This is the naive implementation; you should extend it to eliminate
     // unnecessary JUMP nodes
     Tree.Stm aStm = a.unCx(tt, ff);
+
+    if (aStm instanceof Tree.JUMP) {
+      Tree.JUMP aJump = (Tree.JUMP)aStm;
+      if (aJump.exp instanceof Tree.NAME) {
+          Tree.NAME aName = (Tree.NAME)aJump.exp;
+          aStm = null;
+          t = aName.label;
+      }
+  }
+
     Tree.Stm bStm = b.unCx(tt, ff);
+
+    if (bStm instanceof Tree.JUMP) {
+      Tree.JUMP bJump = (Tree.JUMP)bStm;
+      if (bJump.exp instanceof Tree.NAME) {
+          Tree.NAME bName = (Tree.NAME)bJump.exp; //casting to name
+          bStm = null;
+          f = bName.label;
+      }
+  }
 
     Tree.Stm condStm = cond.unCx(t, f);
 
@@ -34,12 +53,45 @@ class IfThenElseExp extends Exp {
   }
 
   Tree.Exp unEx() {
-    // You must implement this function
-    return new Tree.CONST(0);
+    Temp temp = new Temp();
+    
+    Tree.Exp aExp = a.unEx();
+    if (aExp == null)
+        return null;
+        
+    Tree.Exp bExp = b.unEx();
+    if (bExp == null)
+        return null;
+        
+    else
+      return new Tree.ESEQ(new Tree.SEQ(new Tree.SEQ(cond.unCx(t, f), new Tree.SEQ(new Tree.SEQ(new Tree.LABEL(t), new Tree.SEQ(new Tree.MOVE(new Tree.TEMP(temp), aExp), new Tree.JUMP(join))), new Tree.SEQ(new Tree.LABEL(f), new Tree.SEQ(new Tree.MOVE(new Tree.TEMP(temp), bExp), new Tree.JUMP(join))))), new Tree.LABEL(join)), new Tree.TEMP(temp)); 
   }
 
   Tree.Stm unNx() {
-    // You must implement this function
-    return null;
+    Tree.Stm aStm = a.unNx();
+    
+    if (aStm == null)
+        t = join;
+    else
+    
+        aStm = new Tree.SEQ(new Tree.SEQ(new Tree.LABEL(t), aStm), new Tree.JUMP(join));
+    
+    Tree.Stm bStm = b.unNx();
+    if (bStm == null)
+        f = join;
+    else
+        bStm = new Tree.SEQ(new Tree.SEQ(new Tree.LABEL(f), bStm), new Tree.JUMP(join));
+    
+    if ((aStm == null) && (bStm == null))
+        return cond.unNx();
+        
+    Tree.Stm condStm = cond.unCx(t, f);
+    
+    if (aStm == null)
+        return new Tree.SEQ(new Tree.SEQ(condStm, bStm), new Tree.LABEL(join)); 
+    if (bStm == null)
+        return new Tree.SEQ(new Tree.SEQ(condStm, aStm), new Tree.LABEL(join)); 
+    else
+        return new Tree.SEQ(new Tree.SEQ(condStm, new Tree.SEQ(aStm, bStm)), new Tree.LABEL(join));  
   }
 }
---
+++ test.c
@@ -0,0 +1,6 @@
+let
+  var x := 10
+  var y := 20
+in
+  print(x + y)
+end
